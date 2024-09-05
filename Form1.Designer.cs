9aa62e03bbb940c1adf5692bbf863402f9732dd3
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-09-05 14:56:06 -0400

    7. Primer hola mundo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblHolamundo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHolamundo
+            // 
+            this.lblHolamundo.BackColor = System.Drawing.Color.Gold;
+            this.lblHolamundo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblHolamundo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.lblHolamundo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblHolamundo.Location = new System.Drawing.Point(0, 0);
+            this.lblHolamundo.Name = "lblHolamundo";
+            this.lblHolamundo.Size = new System.Drawing.Size(800, 450);
+            this.lblHolamundo.TabIndex = 0;
+            this.lblHolamundo.Text = "Hola Mundo";
+            this.lblHolamundo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblHolamundo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHolamundo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblHolamundo;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace CursoCsharp
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
